000ffd06a4ad1ccecad03794a0c8a97d590d84ec
Ryan <90898609+Ryan5290@users.noreply.github.com>
2026-09-18 16:04:08 +1200

    split characters into symbols and numbers

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const letters =  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const numbers =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols =  ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 const passwordLength = 15
+
+let characters = Array.from(letters);
 
 let passwordOneEl = document.getElementById("password-1")
 let passwordTwoEl = document.getElementById("password-2")
 
 
 function generatePassword() {
+    buildCharacters()
     let charsLen = characters.length
     let password = ""
     
@@ -24,13 +28,21 @@ function displayPasswords() {
     
     passwordOneEl.textContent = passwordOne
     passwordTwoEl.textContent = passwordTwo
+
+    buildCharacters()
 }
 
 function buildCharacters() {
     let numEl = document.getElementById("num-el")
-    console.log(numEl)
+    let symEl = document.getElementById("sym-el")
+
+    if (numEl.checked) {
+        characters = characters.concat(numbers);
+    }
 }
 
-buildCharacters()
+
+
+
 
 
